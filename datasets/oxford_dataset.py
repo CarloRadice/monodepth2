70171170f7302d59b78e38e3260b13218d143966
carloradice <c.radice@campus.unimib.it>
2021-10-26 15:40:46 +0200

    new way of calculate matrix with cropped images

--- datasets/oxford_dataset.py
+++ datasets/oxford_dataset.py
@@ -14,27 +14,48 @@ class OXFORDDataset(MonoDataset):
     def __init__(self, *args, **kwargs):
         super(OXFORDDataset, self).__init__(*args, **kwargs)
 
-    # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size.
-    # To normalize you need to scale the first row by 1 / image_width and the second row
-    # by 1 / image_height. Monodepth2 assumes a principal point to be exactly centered.
-    # If your principal point is far from the center you might need to disable the horizontal
-    # flip augmentation.
+        # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size.
+        # To normalize you need to scale the first row by 1 / image_width and the second row
+        # by 1 / image_height. Monodepth2 assumes a principal point to be exactly centered.
+        # If your principal point is far from the center you might need to disable the horizontal
+        # flip augmentation.
 
-        # Cosa ho fatto:
-        # prima riga: 983.044006 / 1280, 0, 643.646973/ 1280, 0
-        # seconda riga: 0, 983.044006 / 960, 493.378998 / 960, 0
-        # self.K = np.array([[0.77, 0, 0.5, 0],
-        #                    [0, 1.02, 0.5, 0],
-        #                    [0, 0, 1, 0],
-        #                    [0, 0, 0, 1]], dtype=np.float32)
+        # Monodepth2 assumes a principal point to be exactly centered
+        # stereo wide
+        # intrinsics:  983.044006,  983.044006, 643.646973, 493.378998
+        # se croppo fx e fy rimangono gli stessi
+        # vengono modificati cx e cy secondo questo
+        # https://github.com/BerkeleyAutomation/perception/blob/6b7bfadae206b130dce21b63034d70211ba7a9f8/perception/camera_intrinsics.py#L184
+        width = 1280
+        height = 960
 
-        # NO IMAGE CROP
-        # self.full_res_shape = (1280, 960)
-        # IMAGE CROP
-        self.full_res_shape = (1280, 800)
-        # seconda riga: 0, 983.044006 / 800, 493.378998 / 800, 0
-        self.K = np.array([[0.77, 0, 0.5, 0],
-                           [0, 1.23, 0.62, 0],
+        fx = 983.044006
+        fx /= width
+        fy = 983.044006
+        fy /= height
+
+        cx = 643.646973
+        cy = 493.378998
+        # Parameters
+        # ----------
+        # crop_height : int
+        #     height of crop window
+        # crop_width : int
+        #     width of crop window
+        # crop_ci : int
+        #     row of crop window center
+        # crop_cj : int
+        #     col of crop window center
+        self.crop_area = (0, 300, 1280, 760)
+        crop_width = self.crop_area[2] - self.crop_area[0]
+        crop_height = self.crop_area[3] - self.crop_area[1]
+        crop_ci = self.crop_area[3] - (crop_height / 2)
+        crop_cj = self.crop_area[2] - (crop_width / 2)
+        crop_cx = cx + float(crop_width-1)/2 - crop_cj
+        crop_cy = cy + float(crop_height-1)/2 - crop_ci
+
+        self.K = np.array([[fx, 0, crop_cx, 0],
+                           [0, fy, crop_cy, 0],
                            [0, 0, 1, 0],
                            [0, 0, 0, 1]], dtype=np.float32)
 
@@ -54,10 +75,12 @@ class OXFORDDataset(MonoDataset):
         color = self.loader(self.get_image_path(folder, frame_index, side))
 
         if not self.transform == None:
-            color = self.transform(color)
+            color = self.transform(color, self.crop_area)
 
-        if do_flip:
-            color = color.transpose(pil.FLIP_LEFT_RIGHT)
+        # If your principal point is far from the center you might need to disable the horizontal
+        # flip augmentation.
+        # if do_flip:
+        #     color = color.transpose(pil.FLIP_LEFT_RIGHT)
 
         return color
 
